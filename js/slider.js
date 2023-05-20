af364c969f9cc8fa66f721a6a67972c99301672e
Mariam Sebua <70912461+Sebuamari@users.noreply.github.com>
2023-05-20 21:59:15 +0400

    feat: added plans section

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,11 @@
 const slideButtonLeft = document.querySelector("#arrow-left");
 const slideButtonRight = document.querySelector("#arrow-right");
+const checkBox = document.querySelector('.choice_switcher .switch input');
+const plansHeader = document.querySelector(".plans_header");
+const plansHeaderBase = document.querySelector(".plans_header_base");
+const plansHeaderBundle = document.querySelector(".plans_header_bundle");
+const plansBase = document.querySelector(".base_plans");
+const plansBundle = document.querySelector(".bundle_plans");
 
 let slideOffset = 0;
 const slideAmount = 300;
@@ -16,4 +22,12 @@ slideButtonRight.addEventListener("click", () => {
 
 document.querySelector("#arrow-down").addEventListener("click", () => {
   document.querySelector("#about_hulu").scrollIntoView({ behavior: 'smooth' })
+});
+
+checkBox.addEventListener("change", () => {
+  plansHeader.classList.toggle("bundle");
+  plansHeaderBase.classList.toggle("active");
+  plansHeaderBundle.classList.toggle("active");
+  plansBase.classList.toggle("active");
+  plansBundle.classList.toggle("active");
 });
